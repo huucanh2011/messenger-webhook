9ad9c942f0e2e73d27437d46a9401af61a11650c
huucanh2011 <huucanh2011@github.com>
2020-04-26 18:06:22 +0700

    Fix deploy

--- helpers/index.js
+++ helpers/index.js
@@ -3,15 +3,23 @@ const axios = require("axios");
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const pageUrl = process.env.PAGE_URL;
 
-const callerAPI = (url, method = "GET", data = {}) => {
-  return axios({
-    method: method,
-    url: url,
-    data: data,
-    responseType: "json",
-    headers: {
-      "Content-type": "application/json",
-    },
+const callerAPI = async (url, method = "GET", data = {}) => {
+  return new Promise((reslove, reject) => {
+    axios({
+      method: method,
+      url: url,
+      data: data,
+      responseType: "json",
+      headers: {
+        "Content-type": "application/json",
+      },
+    })
+      .then((res) => {
+        reslove(res);
+      })
+      .catch((err) => {
+        reject(err.response);
+      });
   });
 };
 
